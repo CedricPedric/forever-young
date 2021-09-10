eee4790fd2e4cdbcabea8c82d7462865b385bd6c
CedricPedric <99059069@mydavinci.nl>
2021-09-10 13:35:28 +0200

    tafel manieren

---
+++ tafelmanieren/tafelmanieren.py
@@ -0,0 +1,17 @@
+a = int(input("Geef een getal voor de tafel: "))
+for b in range(1,11):
+    print(b * a)
+
+for x in range(30, 0, -1):
+   print(str(x))
+   print("De raket lanceerd")
+
+
+for c in range(24):
+    if c >= 13:
+        print(str(c) + " PM")
+    else:
+        print(str(c) + " AM")
+
+for d in range(20,51,1):
+    print(d)
