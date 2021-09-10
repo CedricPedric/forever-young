148ee77143e4c3a7d1c75344e65dbf55ed312cd8
CedricPedric <99059069@mydavinci.nl>
2021-09-10 19:10:41 +0200

    Eerste Commit

---
+++ robot-arm/example4.py
@@ -0,0 +1,27 @@
+from RobotArm import RobotArm
+
+robotArm = RobotArm('exercise 4')
+
+# Jouw python instructies zet je vanaf hier:
+for a in range(2):
+    robotArm.grab()
+    robotArm.moveRight()
+    robotArm.moveRight()
+    robotArm.drop()
+    robotArm.moveLeft()
+    robotArm.moveLeft()
+
+robotArm.grab()
+robotArm.moveRight()
+robotArm.drop()
+
+for b in range(2):
+    robotArm.moveRight()
+    robotArm.grab()
+    robotArm.moveLeft()
+    robotArm.drop()
+
+
+
+# Na jouw code wachten tot het sluiten van de window:
+robotArm.wait()
